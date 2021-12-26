ad7c847476df7c9dce07ff8239d70785bd95870f
Jim Rybarski <jim@rybarski.com>
2021-12-26 12:23:59 -0600

    Add basic setup, loop, teardown and dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,70 @@
-fn main() {
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{error::Error, io};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    Terminal,
+};
+
+
+enum Mode {
+    SequenceViewer,
+}
+
+
+struct App {
+    mode: Mode
+}
+
+
+impl App {
+    fn new() -> App {
+        App {
+            mode: Mode::SequenceViewer
+        }
+    }
+}
+
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<()> {
+    loop {
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('q') => return Ok(()),
+                _ => {}
+            }
+        }
+    }
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // run app
+    let mut app = App::new();
+    let res = run_app(&mut terminal, &mut app);
+
+    // shut down terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
+    Ok(())
 }
