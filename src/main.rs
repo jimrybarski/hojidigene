ced44e34f74dbde31a0fdd6edbbe4f2a63fd042b
Jim Rybarski <jim@rybarski.com>
2021-12-30 15:14:25 -0600

    Basic concept working

--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,77 @@ use crossterm::{
 use std::{error::Error, io};
 use tui::{
     backend::{Backend, CrosstermBackend},
-    Terminal,
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    widgets::{Block, Paragraph, Tabs, Wrap},
+    Frame, Terminal,
 };
 
-
+#[derive(PartialEq, Debug)]
 enum Mode {
+    Quit,
     SequenceViewer,
 }
 
-
 struct App {
-    mode: Mode
+    mode: Mode,
 }
-
 
 impl App {
     fn new() -> App {
         App {
-            mode: Mode::SequenceViewer
+            mode: Mode::SequenceViewer,
         }
     }
 }
 
+fn render_sequence_viewer<B: Backend>(f: &mut Frame<B>, app: &App) {
+    let size = f.size();
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(0)
+        .constraints([Constraint::Length(3), Constraint::Min(0)].as_ref())
+        .split(size);
+
+    let block = Block::default().style(Style::default().bg(Color::Black).fg(Color::White));
+    f.render_widget(block, size);
+    let paragraph = Paragraph::new("ACGGATCACTGATCGGTTTACGTACGTACGTACGATCGATCGTACGTACGATCGATCGATCGTACGTACGTACGTACGTACGTACGTACGTACGTACGACGGGGAGCGGCCGCTATAATATATTACGCAGACTATCGGCGCTATACTATATCGATGCTACGTACGTACGTACGATCGATCGTACGATCGTACGTACGTACGTACGATATATCATCGTACGTACGT")
+        .style(Style::default().bg(Color::Black).fg(Color::White))
+        .alignment(Alignment::Left)
+        .wrap(Wrap { trim: true });
+
+    f.render_widget(paragraph, chunks[1]);
+}
+
+fn action_sequence_viewer(app: &mut App, key: KeyCode) -> Mode {
+    assert_eq!(app.mode, Mode::SequenceViewer);
+    match key {
+        KeyCode::Esc => Mode::Quit,
+        _ => Mode::SequenceViewer,
+    }
+}
 
 fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<()> {
+    // Start up in Sequence Viewer mode as the default
+    terminal.draw(|f| render_sequence_viewer(f, &app))?;
+
     loop {
         if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') => return Ok(()),
-                _ => {}
-            }
+            // use the keypress to decide whether to edit the state
+            // of the App and/or change the Mode
+            app.mode = match app.mode {
+                Mode::SequenceViewer => action_sequence_viewer(app, key.code),
+                Mode::Quit => Mode::Quit,
+            };
+            let render_func = match app.mode {
+                Mode::SequenceViewer => render_sequence_viewer,
+                Mode::Quit => return Ok(()),
+            };
+
+            terminal.draw(|f| render_func(f, &app))?;
         }
     }
 }
-
 
 fn main() -> Result<(), Box<dyn Error>> {
     // setup terminal
@@ -62,6 +99,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     )?;
     terminal.show_cursor()?;
 
+    // in case there was an error, print the raw error message
+    // this should effectively never happen
     if let Err(err) = res {
         println!("{:?}", err)
     }
